478a6fba1472a13edfba5833e5cfeccef4381f47
marshadkhn <marshadkhn89@gmail.com>
2025-09-17 00:35:24 +0530

    Did Something in contact us page

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,13 +1,20 @@
 import * as React from "react";
 import { cn } from "@/utils/utils";
-import HeaderContent, { type HeaderContentProps } from "@/components/headerContent";
+import HeaderContent, {
+  type HeaderContentProps,
+} from "@/components/headerContent";
 import Container from "@/components/Container";
 
 interface HeroProps extends HeaderContentProps {
   backgroundUrl?: string;
+  backgroundColor?: string;
 }
 
-export default function Hero({ backgroundUrl, ...content }: HeroProps) {
+export default function Hero({
+  backgroundUrl,
+  backgroundColor,
+  ...content
+}: HeroProps) {
   return (
     <section
       id="home"
@@ -24,8 +31,14 @@ export default function Hero({ backgroundUrl, ...content }: HeroProps) {
           className="pointer-events-none absolute inset-0 -z-10 bg-cover bg-center opacity-70"
           style={{ backgroundImage: `url(${backgroundUrl})` }}
         />
+      ) : backgroundColor ? (
+        <div
+          aria-hidden
+          className="pointer-events-none absolute inset-0 -z-10"
+          style={{ backgroundColor }}
+        />
       ) : null}
-  <div className="absolute inset-0 -z-10 bg-gradient-to-b from-black/70 via-black/60 to-black/80 md:from-black/60 md:via-black/40 md:to-black/70" />
+      <div className="absolute inset-0 -z-10 bg-gradient-to-b from-black/70 via-black/60 to-black/80 md:from-black/60 md:via-black/40 md:to-black/70" />
       <Container>
         <HeaderContent {...content} />
       </Container>
